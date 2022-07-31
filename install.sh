3bca7f2c638fc7595fbb1d86fc14929daa1f9bca
Matheus Tavares <matheustavarestrindade@hotmail.com>
2022-07-31 13:26:52 -0300

    Fixed typo on install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ cd $CURRENT_DIR
 mvn package
 echo "Installing library..."
 # Maven install command fixed for windows path
-mvn install:install-file -Dfile="$(cygpath -w $CURRENT_DIR)\target\java-smartctl-1.0-SNAPSHOT.jar" -DgroupId=com.smartctl -DartifactId=smartctl -Dversion=1.0-SNAPSHOT -Dpackaging=jar
+mvn install:install-file -Dfile="$(cygpath -w $CURRENT_DIR)\target\java-smartctl-1.0-SNAPSHOT.jar" -DgroupId="com.matheustt.smartctl" -DartifactId="java-smartctl" -Dversion=1.0-SNAPSHOT -Dpackaging=jar
